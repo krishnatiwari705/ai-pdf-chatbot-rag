dfab51b7b10e22ea15d324d69f07796fc546ad51
Krishna Tiwari <kt817483@gmail.com>
2026-08-07 22:58:48 +0530

    Fix CORS URLs

--- src/app.js
+++ src/app.js
@@ -10,23 +10,12 @@ const ragRoutes = require("./routes/rag.routes");
 
 const app = express();
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://ai-pdf-chatbot-rag-gamma.vercel.app",
-];
-
 app.use(
   cors({
-    origin: function (origin, callback) {
-      // Allow requests with no origin (Postman, server-to-server)
-      if (!origin) return callback(null, true);
-
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      }
-
-      return callback(new Error("Not allowed by CORS"));
-    },
+    origin: [
+      "http://localhost:5173",
+      "https://ai-pdf-chatbot-rag-gamma.vercel.app",
+    ],
     credentials: true,
   })
 );
